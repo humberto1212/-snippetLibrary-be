ea8818912291a552bb4c61f5f09703fa55873a2a
humberto1212 <angeluccirincon@gmail.com>
2022-06-08 16:40:13 +0200

    create SnippetRepository

---
+++ src/main/java/com/example/snippet/snippet/SnippetRepository.java
@@ -0,0 +1,10 @@
+package com.example.snippet.snippet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SnippetRepository extends JpaRepository<Snippet, Long>{
+
+    
+}
