96258c0fa6f820d7f19b6d37eaa3a5b0341fa2a3
humberto1212 <angeluccirincon@gmail.com>
2022-06-10 16:58:31 +0200

    handle crossOrigin error, add PostMapping

--- src/main/java/com/example/snippet/snippet/SnippetController.java
+++ src/main/java/com/example/snippet/snippet/SnippetController.java
@@ -3,7 +3,10 @@ package com.example.snippet.snippet;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,9 +21,16 @@ public class SnippetController {
         this.snippetService = snippetService;
     }
 
+    @CrossOrigin
     @GetMapping
 	public List<Snippet> getSnippetCo() {
         return snippetService.getSnippet(); 
 	}
+
+    @CrossOrigin
+    @PostMapping
+    public void registerNewSnippet(@RequestBody Snippet snippet){
+        snippetService.addNewSnippet(snippet);
+    }
     
 }
