8e825c9ccfda5d554a998498815754a3e11735b1
humberto1212 <angeluccirincon@gmail.com>
2022-06-10 16:59:16 +0200

    add search for title

--- src/main/java/com/example/snippet/snippet/SnippetRepository.java
+++ src/main/java/com/example/snippet/snippet/SnippetRepository.java
@@ -1,10 +1,15 @@
 package com.example.snippet.snippet;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SnippetRepository extends JpaRepository<Snippet, Long>{
-
+    
+    @Query("SELECT s FROM Snippet s WHERE s.title = ?1")
+    Optional<Snippet> findSnippetByTitle(String title);
     
 }
