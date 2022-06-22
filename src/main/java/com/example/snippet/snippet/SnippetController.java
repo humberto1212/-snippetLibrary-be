10e4edd603fd3689f4eae6dab66f8fdf4fa4c060
humberto1212 <angeluccirincon@gmail.com>
2022-06-22 17:40:54 +0200

    create  deleteSnippet

--- src/main/java/com/example/snippet/snippet/SnippetController.java
+++ src/main/java/com/example/snippet/snippet/SnippetController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,12 @@ public class SnippetController {
     @PostMapping
     public void registerNewSnippet(@RequestBody Snippet snippet){
         snippetService.addNewSnippet(snippet);
+    }
+
+    @CrossOrigin
+    @DeleteMapping(path = "{snippetId}")
+    public void deleteSnippet(@PathVariable("snippetId") long snippetId){
+        snippetService.deleteSnippetSe(snippetId);
     }
     
 }
